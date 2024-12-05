f28ecc53cacb2c1c4461bc008a5b79daa27b1334
Theotime Faure <theotimefaure@free.fr>
2024-12-05 11:39:16 +0100

    creation de 4 carres

--- mcarre.cpp
+++ mcarre.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 void main()
 {
-	CCarre carre1;
+	CCarre carre[4];
 	int decalage_horizontale;
 	int decalage_verticale;
 	int longueur_cote;
@@ -35,17 +35,29 @@ void main()
 	cout << "Entrez la deuxieme coordonner du vecteur : ";
 	cin >> dy;
 
+	carre[0].Setcote(longueur_cote);
+	carre[1].Setcote(longueur_cote);
+	carre[2].Setcote(longueur_cote);
+	carre[3].Setcote(longueur_cote);
 	
-	carre1.Setsx(decalage_horizontale);
-	carre1.Setsy(decalage_verticale);
-	carre1.Setcote(longueur_cote);
+	carre[0].Setsx(decalage_horizontale);
+	carre[0].Setsy(decalage_verticale);
+	carre[1].Setsx(decalage_horizontale);
+	carre[1].Setsy(decalage_verticale);
+	carre[2].Setsx(decalage_horizontale);
+	carre[2].Setsy(decalage_verticale);
+	carre[3].Setsx(decalage_horizontale);
+	carre[3].Setsy(decalage_verticale);
 
-	carre1.Deplacer(direction, saut);
-	cout << "Avec la direction et le saut\nsx = " << carre1.Getsx() << "\tsy = " << carre1.Getsy() << "\tcote = " << carre1.GetCote() << endl;
+	carre[0].Deplacer(direction, saut);
+	cout << "carre 1\nsx = " << carre[0].Getsx() << "\tsy = " << carre[0].Getsy() << "\tcote = " << carre[0].GetCote() << endl;
 
-	carre1.Setsx(decalage_horizontale);
-	carre1.Setsy(decalage_verticale);
-	carre1.Deplacer(dx, dy);
-	cout << "Avec le vecteur\nsx = " << carre1.Getsx() << "\tsy = " << carre1.Getsy() << "\tcote = " << carre1.GetCote() << endl;
+	carre[1].Deplacer(dx, dy);
+	cout << "carre 2\nsx = " << carre[1].Getsx() << "\tsy = " << carre[1].Getsy() << "\tcote = " << carre[1].GetCote() << endl;
 
+	carre[2].Deplacer('o', saut+10);
+	cout << "carre 3\nsx = " << carre[2].Getsx() << "\tsy = " << carre[2].Getsy() << "\tcote = " << carre[2].GetCote() << endl;
+	
+	carre[3].Deplacer(dx+5, dy+5);
+	cout << "carre 4\nsx = " << carre[3].Getsx() << "\tsy = " << carre[3].Getsy() << "\tcote = " << carre[3].GetCote() << endl;
 }
